5b6dc71e36f6ae5eae7fa3c244dff8310a280141
Emil Fresk <emil.fresk@gmail.com>
2020-07-22 10:22:45 +0200

    Testing iterators

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,10 @@
 //! assert_eq!(ret, y);
 //! ```
 
-#![no_std]
+// #![no_std]
 #![deny(missing_docs)]
 
+use core::iter::{DoubleEndedIterator, ExactSizeIterator};
 use core::mem::size_of;
 use core::ops::{Bound, RangeBounds};
 
@@ -71,14 +72,14 @@ macro_rules! impl_bit_range_slice {
             #[cfg_attr(feature = "enable-inline", inline)]
             #[cfg_attr(feature = "never-inline", inline(never))]
             fn range_read_le<R: RangeBounds<usize>>(self, range: R) -> $numeric {
-                let res: u64 = bit_range_read_le_impl(&self, range);
+                let res: u64 = bit_range_read_le_iter_impl(self.iter(), range);
                 res as $numeric
             }
 
             #[cfg_attr(feature = "enable-inline", inline)]
             #[cfg_attr(feature = "never-inline", inline(never))]
             fn range_read_be<R: RangeBounds<usize>>(self, range: R) -> $numeric {
-                let res: u64 = bit_range_read_be_impl(&self, range);
+                let res: u64 = bit_range_read_le_iter_impl(self.iter().rev(), range);
                 res as $numeric
             }
         })*
@@ -94,13 +95,13 @@ macro_rules! impl_bit_range_write_slice {
             #[cfg_attr(feature = "enable-inline", inline)]
             #[cfg_attr(feature = "never-inline", inline(never))]
             fn range_write_le<R: RangeBounds<usize>>(self, range: R, value: $numeric) {
-                bit_range_write_le_impl(self, value as u64, range)
+                write_le_compound(self, value as u64, range);
             }
 
             #[cfg_attr(feature = "enable-inline", inline)]
             #[cfg_attr(feature = "never-inline", inline(never))]
             fn range_write_be<R: RangeBounds<usize>>(self, range: R, value: $numeric) {
-                bit_range_write_be_impl(self, value as u64, range)
+                write_be_compound(self, value as u64, range);
             }
         })*
     }
@@ -111,7 +112,10 @@ impl_bit_range_write_slice!(u8, u16, i32, u32, u64,);
 
 /// Helper of common code
 #[inline(always)]
-fn setup<R: RangeBounds<usize>>(input: &[u8], range: R) -> (usize, usize, usize, usize) {
+fn setup_iter<'a, R>(input_len: usize, range: R) -> (usize, usize, usize, usize)
+where
+    R: RangeBounds<usize>,
+{
     let start_bit = match range.start_bound() {
         Bound::Included(start) => *start,
         Bound::Excluded(start) => *start + 1,
@@ -124,54 +128,41 @@ fn setup<R: RangeBounds<usize>>(input: &[u8], range: R) -> (usize, usize, usize,
     };
     let total_bits = end_bit - start_bit + 1;
     let start_byte = start_bit / 8;
-    let end_byte = (end_bit / 8).min(input.len());
+    let end_byte = (end_bit / 8).min(input_len);
 
     let start_bit = start_bit - start_byte * 8;
 
     (start_bit, total_bits, start_byte, end_byte)
 }
 
-// try 2
 #[inline]
-fn bit_range_read_le_impl<R: RangeBounds<usize>>(input: &[u8], range: R) -> u64 {
-    let (start_bit, total_bits, start_byte, end_byte) = setup(input, range);
+fn bit_range_read_le_iter_impl<'a, I, R>(input: I, range: R) -> u64
+where
+    I: Iterator<Item = &'a u8> + DoubleEndedIterator + ExactSizeIterator,
+    R: RangeBounds<usize>,
+{
+    let (start_bit, total_bits, start_byte, end_byte) = setup_iter(input.len(), range);
 
-    // Check safety
-    assert!(total_bits <= 64);
+    let iter = input.skip(start_byte).take(end_byte + 1);
 
     // The rust compiler is smart enough to see through this and not so u128 operations.
     let mask = (1 << total_bits) - 1;
-    let mut output = read_le_u128(unsafe { get_unchecked(start_byte, end_byte + 1, input) });
+    let mut output = read_u128_le(iter);
     output >>= start_bit;
     output &= mask;
 
     output as u64
 }
 
-#[inline]
-fn bit_range_read_be_impl<R: RangeBounds<usize>>(input: &[u8], range: R) -> u64 {
-    let (start_bit, total_bits, start_byte, end_byte) = setup(input, range);
-
-    // Check safety
-    assert!(total_bits <= 64);
-
-    // The rust compiler is smart enough to see through this and not so u128 operations.
-    let mask = (1 << total_bits) - 1;
-    let mut output = read_be_u128(unsafe { get_unchecked(start_byte, end_byte + 1, input) });
-    output >>= start_bit;
-    output &= mask;
-
-    output as u64
-}
-
-#[inline]
-fn bit_range_write_le_impl<R: RangeBounds<usize>>(output: &mut [u8], val: u64, range: R) {
-    let (start_bit, total_bits, start_byte, end_byte) = setup(output, range);
-
-    assert!(total_bits <= 64);
+fn write_le_compound<R>(output: &mut [u8], val: u64, range: R)
+where
+    R: RangeBounds<usize>,
+{
+    let (start_bit, total_bits, start_byte, end_byte) = setup_iter(output.len(), range);
+    let iter = output.iter().skip(start_byte).take(end_byte + 1);
 
     // Extract area as u128
-    let mut work_value = read_le_u128(unsafe { get_unchecked(start_byte, end_byte + 1, output) });
+    let mut work_value = read_u128_le(iter);
 
     let mask = ((1 << total_bits) - 1) << start_bit;
     let val = ((val as u128) << start_bit) & mask;
@@ -180,18 +171,21 @@ fn bit_range_write_le_impl<R: RangeBounds<usize>>(output: &mut [u8], val: u64, r
     work_value &= !mask;
     work_value |= val;
 
+    let iter = output.iter_mut().skip(start_byte).take(end_byte + 1);
+
     // Write area back
-    unsafe { write_value_le(start_byte, end_byte + 1, output, work_value) };
+    write_value_le(iter, work_value);
 }
 
-#[inline]
-fn bit_range_write_be_impl<R: RangeBounds<usize>>(output: &mut [u8], val: u64, range: R) {
-    let (start_bit, total_bits, start_byte, end_byte) = setup(output, range);
-
-    assert!(total_bits <= 64);
+fn write_be_compound<R>(output: &mut [u8], val: u64, range: R)
+where
+    R: RangeBounds<usize>,
+{
+    let (start_bit, total_bits, start_byte, end_byte) = setup_iter(output.len(), range);
+    let iter = output.iter().rev().skip(start_byte).take(end_byte + 1);
 
     // Extract area as u128
-    let mut work_value = read_be_u128(unsafe { get_unchecked(start_byte, end_byte + 1, output) });
+    let mut work_value = read_u128_le(iter);
 
     let mask = ((1 << total_bits) - 1) << start_bit;
     let val = ((val as u128) << start_bit) & mask;
@@ -200,57 +194,31 @@ fn bit_range_write_be_impl<R: RangeBounds<usize>>(output: &mut [u8], val: u64, r
     work_value &= !mask;
     work_value |= val;
 
+    let iter = output.iter_mut().rev().skip(start_byte).take(end_byte + 1);
+
     // Write area back
-    unsafe { write_value_be(start_byte, end_byte + 1, output, work_value) };
+    write_value_le(iter, work_value);
 }
 
 #[inline(always)]
-unsafe fn get_unchecked<T>(start: usize, end: usize, slice: &[T]) -> &[T] {
-    core::slice::from_raw_parts(slice.as_ptr().add(start), end - start)
-}
-
-#[inline]
-fn read_le_u128(input: &[u8]) -> u128 {
-    let mut ret = 0;
-
-    for b in input.iter().rev() {
-        ret <<= 8;
-        ret |= *b as u128;
-    }
-
-    ret
-}
-
-#[inline]
-fn read_be_u128(input: &[u8]) -> u128 {
-    let mut ret = 0;
-
-    for b in input.iter() {
-        ret <<= 8;
-        ret |= *b as u128;
-    }
-
-    ret
+fn read_u128_le<'a, I>(input: I) -> u128
+where
+    I: Iterator<Item = &'a u8> + DoubleEndedIterator,
+{
+    input.rev().fold(0, |acc, x| (acc << 8) | *x as u128)
 }
 
 #[inline(always)]
-unsafe fn write_value_le(start: usize, end: usize, output: &mut [u8], value: u128) {
-    let val_as_bytes = &value.to_le_bytes();
-    core::ptr::copy_nonoverlapping(
-        val_as_bytes.as_ptr(),
-        output.as_mut_ptr().add(start),
-        end - start,
-    );
-}
-
-#[inline(always)]
-unsafe fn write_value_be(start: usize, end: usize, output: &mut [u8], value: u128) {
+fn write_value_le<'a, O>(output: O, value: u128)
+where
+    O: Iterator<Item = &'a mut u8> + DoubleEndedIterator,
+{
     let val_as_bytes = &value.to_be_bytes();
-    core::ptr::copy_nonoverlapping(
-        val_as_bytes.as_ptr(),
-        output.as_mut_ptr().add(start),
-        end - start,
-    );
+    val_as_bytes
+        .iter()
+        .rev()
+        .zip(output)
+        .for_each(|(i, o)| *o = *i);
 }
 
 #[cfg(test)]
@@ -258,55 +226,4 @@ unsafe fn write_value_be(start: usize, end: usize, output: &mut [u8], value: u12
 extern crate std;
 
 #[cfg(test)]
-mod tests {
-    use crate::*;
-
-    #[test]
-    fn write_range() {
-        let y = &mut [0b11111111u8, 0b11111111, 0b11111111, 0b11111111];
-        let val = &[0b00001111u8, 0b11110000, 0b11111111, 0b11111111];
-
-        // println!("");
-
-        y.range_write_le(4..12, 0);
-
-        // for (i, b) in y.iter().enumerate() {
-        //     println!("{}: {:#010b}", i, *b);
-        // }
-
-        assert_eq!(&y, &val);
-    }
-
-    #[test]
-    fn endian_check() {
-        let y: u32 = 0b00001111_11110000_01010000_00001010;
-        let y_arr = &[0b00001010u8, 0b01010000, 0b11110000, 0b00001111];
-        assert_eq!(&y.to_le_bytes(), y_arr);
-
-        let v: u32 = y_arr.range_read_le(..);
-        assert_eq!(v, y);
-    }
-
-    #[test]
-    fn read_range() {
-        // let x: u32 = 0b000111100;
-
-        // println!("");
-        // println!("x: {:b}", x);
-        // println!("1: {:b}", x.range_read(..));
-        // println!("2: {:b}", x.range_read(..3));
-        // println!("3: {:b}", x.range_read(..=3));
-        // println!("4: {:b}", x.range_read(1..));
-        // // println!("{:b}", x.range_read(18446744073709551615..));
-        // println!("5: {:b}", x.range_read(2..5));
-        // println!("6: {:b}", x.range_read(2..=5));
-        // // println!("{:b}", x.range_read(2..=5234234234234234));
-
-        // let y: u32 = 0b00001111_11110001_11010011_00001110;
-        let y = &[0b00001110u8, 0b11010011, 0b11110001, 0b10001111];
-
-        let z3: u64 = y.range_read_le(8..24);
-
-        assert_eq!(z3, 0b1111000111010011);
-    }
-}
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,63 @@
+use crate::*;
+
+#[test]
+fn write_range_be() {
+    let y = &mut [0b11111111u8, 0b11111111, 0b11111111, 0b11111111];
+    let val = &[0b11111111u8, 0b11111111, 0b11110000, 0b00001111];
+
+    y.range_write_be(4..12, 0);
+
+    assert_eq!(&y, &val);
+}
+
+#[test]
+fn write_range_le() {
+    let y = &mut [0b11111111u8, 0b11111111, 0b11111111, 0b11111111];
+    let val = &[0b00001111u8, 0b11110000, 0b11111111, 0b11111111];
+
+    y.range_write_le(4..12, 0);
+
+    assert_eq!(&y, &val);
+}
+
+#[test]
+fn endian_check_le() {
+    let y_le: u32 = 0b00001111_11110000_01010000_00001010;
+    let y_arr = &[0b00001010u8, 0b01010000, 0b11110000, 0b00001111];
+    assert_eq!(&y_le.to_le_bytes(), y_arr);
+
+    let v: u32 = y_arr.range_read_le(..);
+    assert_eq!(v, y_le);
+}
+
+#[test]
+fn endian_check_be() {
+    let y_be: u32 = 0b00001010_01010000_11110000_00001111;
+    let y_arr = &[0b00001010u8, 0b01010000, 0b11110000, 0b00001111];
+    assert_eq!(&y_be.to_be_bytes(), y_arr);
+
+    let v: u32 = y_arr.range_read_be(..);
+    assert_eq!(v, y_be);
+}
+
+#[test]
+fn read_range_be() {
+    // let y_be: u32 = 0b00001010_01010000_11110000_00001111;
+    let y_arr = &[0b00001010u8, 0b01010000, 0b11110000, 0b00001111];
+
+    let r: u32 = y_arr.range_read_be(0..8);
+    assert_eq!(r, 0b00001111);
+}
+
+#[test]
+fn read_range_le() {
+    // let y: u32 = 0b00001111_11110001_11010011_00001110;
+    let y = &[0b00001110u8, 0b11010011, 0b11110001, 0b10001111];
+
+    let z3: u32 = y.range_read_le(8..24);
+
+    assert_eq!(z3, 0b1111000111010011);
+
+    let z3: u32 = y.range_read_le(0..8);
+    assert_eq!(z3, 0b00001110);
+}
